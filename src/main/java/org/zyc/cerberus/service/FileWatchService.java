10146777e6c1c3849b4c4e53978235e9b55d1a1e
zyc_hp_laptop <271933294@qq.com>
2020-04-20 23:24:09 +0800

    fix endless loop bug

--- src/main/java/org/zyc/cerberus/service/FileWatchService.java
+++ src/main/java/org/zyc/cerberus/service/FileWatchService.java
@@ -7,6 +7,7 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 
 import java.io.File;
 import java.io.IOException;
@@ -37,8 +38,16 @@ public class FileWatchService implements ApplicationRunner {
     public void watch(List<String> oriPath, List<String> destPath) throws IOException, InterruptedException {
         WatchService watchService
                 = FileSystems.getDefault().newWatchService();
-        String watchedFilePath = getFilePath(oriPath);
-        String targetFilePath = getFilePath(destPath);
+        String watchedFilePath = getFilePath(oriPath, false);
+        String targetFilePath = getFilePath(destPath, true);
+        boolean needCopy = !StringUtils.isEmpty(targetFilePath);
+        // check path for update event happen one file in a loop
+        if (needCopy &&
+                (watchedFilePath.equals(targetFilePath) || targetFilePath.startsWith(watchedFilePath))) {
+            LOGGER.error("watch file path & target file path is same, please check your config, this may cause " +
+                    "endless loop!!!");
+            throw new InterruptedException("same file path is unacceptable!!");
+        }
         Path path = Paths.get(watchedFilePath);
         path.register(
                 watchService,
@@ -47,19 +56,14 @@ public class FileWatchService implements ApplicationRunner {
                 StandardWatchEventKinds.ENTRY_DELETE
         );
         WatchKey key;
-        while ((key = watchService.take()) != null  && !stop.get()) {
+        while ((key = watchService.take()) != null && !stop.get()) {
             try {
                 for (WatchEvent<?> event : key.pollEvents()) {
                     LOGGER.info("Event kind: {}" + ". File affected: {}", event.kind(), event.context());
-                    if (StandardWatchEventKinds.ENTRY_MODIFY.equals(event.kind())) {
+                    if (StandardWatchEventKinds.ENTRY_MODIFY.equals(event.kind()) && needCopy) {
                         executorService.execute(() -> {
-//                            try {
-//                                Thread.sleep(600);
-//                            } catch (InterruptedException e) {
-//                                e.printStackTrace();
-//                            }
                             File oldFile = new File(watchedFilePath + event.context());
-                            File newFile = new File(targetFilePath + event.context() + imgIndex.getAndIncrement());
+                            File newFile = new File(targetFilePath + System.currentTimeMillis() + event.context());
                             try {
                                 Files.copy(oldFile.toPath(), newFile.toPath());
                                 LOGGER.info("copy file success");
@@ -77,9 +81,13 @@ public class FileWatchService implements ApplicationRunner {
         }
     }
 
-    private String getFilePath(List<String> paths) {
+    private String getFilePath(List<String> paths, boolean isNullable) {
         if (CollectionUtils.isEmpty(paths)) {
-            return watchPath;
+            if (isNullable) {
+                return null;
+            } else {
+                return watchPath;
+            }
         }
         String path = paths.get(0);
         if (path.endsWith("\\") || path.endsWith("/")) {
